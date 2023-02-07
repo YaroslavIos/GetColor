aeddaf732536c91a64c1e1634aed510a7c28d439
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-02-07 22:14:15 +0300

    Writing some remarks.

--- GetColor/ViewController.swift
+++ GetColor/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-// MARK: - Setting IBOutlets
+    // MARK: - Setting IBOutlets
     
     @IBOutlet var viewColor: UIView!
+    
     @IBOutlet var redResult: UILabel!
     @IBOutlet var greenResult: UILabel!
     @IBOutlet var blueResult: UILabel!
@@ -26,52 +27,63 @@ final class ViewController: UIViewController {
         viewColor.layer.cornerRadius = 18
         
     }
-
-// MARK: - Setting IBActions
+    
+    //MARK: - Setting IBAction
     
     func changeColor() {
-        viewColor.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        viewColor.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    // Подключаем redSlider, greenSlider и blueSlider к одному методу
+    
+    @IBAction func SliderAction() {
+        changeColor()
+        
+        let redRoundedResult = Double(round(redSlider.value * 100) / 100)
+        redResult.text = redRoundedResult.formatted()
+        
+        let greenRoundedResult = Double(round(greenSlider.value * 100) / 100)
+        greenResult.text = greenRoundedResult.formatted()
+        
+        let blueRoundedResult = Double(round(blueSlider.value * 100) / 100)
+        blueResult.text = blueRoundedResult.formatted()
     }
 
-    @IBAction func redSliderAction() {
+    // MARK: - The first way of resolving this homework
+    // Каждому UISlider прописывал свой собственный метод)
+ 
+    /*
+     func changeColor() {
+     viewColor.backgroundColor = UIColor(
+     red: CGFloat(redSlider.value),
+     green: CGFloat(greenSlider.value),
+     blue: CGFloat(blueSlider.value),
+     alpha: 1
+     )
+     }
+     
+     @IBAction func redSliderAction() {
         changeColor()
         let roundedResult = Double(round(redSlider.value * 100) / 100)
         redResult.text = roundedResult.formatted()
-    }
-
+        }
+     
     @IBAction func greenSliderAction() {
         changeColor()
         let roundedResult = Double(round(greenSlider.value * 100) / 100)
         greenResult.text = roundedResult.formatted()
-    }
-
+        }
+     
     @IBAction func blueSliderAction() {
         changeColor()
         let roundedResult = Double(round(blueSlider.value * 100) / 100)
         blueResult.text = roundedResult.formatted()
-    }
-    
-// MARK: - Another way solving this project
-    /*
-     Методом научного эксперимента я понял, что все три слайдера можно объединить в один метод и соответственно
-     каждому слайдеру передавать значения.
-     */
-    
-    
-    /*
-     func changeColor() {
-         viewColor.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
-     }
-     
-     
-     // Подключаем redSlider, greenSlider и blueSlider к одному методу
-     
-     @IBAction func SliderAction() {
-         changeColor()
-         let roundedResult = Double(round(changedSlider.value * 100) / 100)
-         sliderResult.text = roundedResult.formatted()
-     }
-     
+        }
      */
     }
 
