4fcf2e8bd6820e863d9948faed82bebc39d96d52
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-02-25 15:43:22 +0300

    Working with files

---
+++ GetColor/MainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainScreenViewController.swift
+//  GetColor
+//
+//  Created by Ярослав Любиченко on 23.2.2023.
+//
+
+import UIKit
+
+protocol SettingsViewControllerDelegate: AnyObject {
+    func updateBackground(for color: UIColor)
+}
+
+final class MainViewController: UIViewController {
+    
+    var color = UIColor(
+        red: 0.5,
+        green: 0.4,
+        blue: 1,
+        alpha: 1
+        )
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = color
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingVC = segue.destination as? SettingsViewController else { return }
+        settingVC.color = color
+        settingVC.delegate = self
+    }
+}
+
+
+extension MainViewController: SettingsViewControllerDelegate {
+    func updateBackground(for color: UIColor) {
+        view.backgroundColor = color
+    }
+}
--- GetColor/SettingsViewController.swift
+++ GetColor/SettingsViewController.swift
@@ -7,9 +7,7 @@
 
 import UIKit
 
-final class SettingsScreenViewController: UIViewController {
-
-    // MARK: - Setting IBOutlets
+final class SettingsViewController: UIViewController {
     
     @IBOutlet var viewColor: UIView!
     
@@ -21,13 +19,15 @@ final class SettingsScreenViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    var backgroundColor: Color!
+    var color: UIColor!
+    unowned var delegate: SettingsViewControllerDelegate!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         viewColor.layer.cornerRadius = 18
         
+        viewColor.backgroundColor = color
         initialViewBackground()
         initialPositionOfSliders()
         takeValuesOfLabels(for: redResult, greenResult, blueResult)
@@ -44,6 +44,12 @@ final class SettingsScreenViewController: UIViewController {
             blueResult.text = string(from: sender)
         }
     }
+    
+    @IBAction func doneButtonPressed() {
+        delegate.updateBackground(for: color)
+        dismiss(animated: true)
+    }
+    
     
     private func changeColor() {
         viewColor.backgroundColor = UIColor(
@@ -65,6 +71,7 @@ final class SettingsScreenViewController: UIViewController {
                 blueResult.text = string(from: blueSlider)
             }
         }
+        print(redResult.text ?? "", greenResult.text ?? "", blueResult.text ?? "")
     }
     
     private func string(from slider: UISlider) -> String {
@@ -73,34 +80,46 @@ final class SettingsScreenViewController: UIViewController {
     
     }
 
-extension SettingsScreenViewController {
-    
-// MARK - Takes a values from a main screen for a view
+
+extension UIColor {
+    var colorValue: CIColor {
+        return CIColor(color: self)
+    }
+
+    var components: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        let colorValue = self.colorValue
+        return(colorValue.red, colorValue.green, colorValue.blue, colorValue.alpha)
+    }
+}
+
+
+extension SettingsViewController {
     
     func initialViewBackground() {
         viewColor.backgroundColor = UIColor(
-            red: CGFloat(backgroundColor.red),
-            green: CGFloat(backgroundColor.green),
-            blue: CGFloat(backgroundColor.blue),
-            alpha: CGFloat(backgroundColor.alpha)
+            red: CGFloat(color.components.red),
+            green: CGFloat(color.components.green),
+            blue: CGFloat(color.components.blue),
+            alpha: CGFloat(color.components.alpha)
         )
     }
-    
+
     func initialPositionOfSliders() {
-        redSlider.value = backgroundColor.red
-        greenSlider.value = backgroundColor.green
-        blueSlider.value = backgroundColor.blue
+        redSlider.value = Float(color.components.red)
+        greenSlider.value = Float(color.components.green)
+        blueSlider.value = Float(color.components.blue)
+        print(redSlider.value, greenSlider.value, blueSlider.value)
     }
-    
+
     func takeValuesOfLabels(for labels: UILabel...) {
         labels.forEach { label in
             switch label {
             case redResult:
-                redResult.text = String(backgroundColor.red)
+                redResult.text = string(from: redSlider)
             case greenResult:
-                greenResult.text = String(backgroundColor.green)
+                greenResult.text = string(from: greenSlider)
             default:
-                blueResult.text = String(backgroundColor.blue)
+                blueResult.text = string(from: blueSlider)
             }
         }
     }
